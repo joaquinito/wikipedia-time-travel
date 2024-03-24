43881081a2d823f479435123242720792820596b
Ricardo Joaquinito <rjoaquinito@gmail.com>
2024-03-24 23:18:54 +0100

    Added conditions to date picker

--- popup/wikipedia_time_travel.js
+++ popup/wikipedia_time_travel.js
@@ -37,42 +37,49 @@ async function extractWikipediaArticleRawName(url) {
 }
 
 async function displayWikipediaArticleData(articleNameRaw) {
-  console.log("Raw name: " + articleNameRaw)
+
   const articleName = decodeURIComponent(articleNameRaw).replace(/_/g, " ")
+
   // Get the creation date of the page
   console.log("Clean name: " + articleName)
   const creationDate = await getCreationDate(articleNameRaw)
+  const dateObj = new Date(creationDate)
+  const dateFormatOptions = { day: "numeric", month: "long", year: "numeric" }
+  const creationDateLongFormat = dateObj.toLocaleDateString("en-GB", dateFormatOptions)
+
+  //Define min and max date in date picker
+  document.getElementById("date-picker").min = creationDate
+  document.getElementById("date-picker").max  = new Date().toISOString().split('T')[0]
+
   // Display the article name and creation date
   document.getElementById("article-name").textContent = articleName
-  document.getElementById("article-creation-date").textContent = "Page created on " + creationDate
+  document.getElementById("article-creation-date").textContent = "Page created on " + creationDateLongFormat
 }
 
 async function getCreationDate(articleName) {
-  // Call to MediaWiki API to get first revision of the page
-  const response = await fetch(MEDIAWIKI_API_GET_FIRST_REVISION + "&titles=" + articleName)
+  try {
+    var response = await fetch(MEDIAWIKI_API_GET_FIRST_REVISION + "&titles=" + articleName)
+  } 
+  catch (error) {
+    console.error("Error fetching data:", error)
+  }
   const data = await response.json()
 
-  // Get timestamp, convert to human-readable format
   var creation_timestamp = data.query.pages[0].revisions[0].timestamp
-  console.log("timestamp:", creation_timestamp)
-  const dateObj = new Date(creation_timestamp)
-  console.log(dateObj)
-  const dateFormatOptions = { day: "numeric", month: "long", year: "numeric" }
-  formattedDate = dateObj.toLocaleDateString("en-GB", dateFormatOptions)
-  console.log(formattedDate)
-  return formattedDate
+  return creation_timestamp.split("T")[0]
+  
 }
 
-async function openArticleinSelectedDate(rawName, date) {
+async function openArticleInSelectedDate(rawName, date) {
+  
   try {
-    var response = fetch(
+    var response = await fetch(
       MEDIAWIKI_API_GET_REVISION + "&titles=" + rawName + "&rvstart=" + date + "T23%3A59%3A59.000Z"
     )
   } catch (error) {
     console.error("Error fetching data:", error)
   }
-
-  const data = response.json()
+  const data = await response.json()
 
   // Parse JSON response and extract the revid
   const revId = data.query.pages[0].revisions[0].revid
@@ -84,6 +91,10 @@ async function openArticleinSelectedDate(rawName, date) {
 
 document.addEventListener("DOMContentLoaded", () => {
   // Get currently open tab
+
+  const submitButton = document.getElementById("submit-button")
+  submitButton.disabled = true;
+
   chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
     var currentUrl = tabs[0].url
     var wikipediaPageRawName = ""
@@ -98,12 +109,26 @@ document.addEventListener("DOMContentLoaded", () => {
       document.getElementById("form-body").style.display = "none"
     }
 
-    const submitButton = document.getElementById("submit-button")
+    
+    const datePicker = document.getElementById("date-picker");
+
     submitButton.addEventListener("click", async () => {
-      const inputDate = document.getElementById("date").value
-      console.log(inputDate)
-      await openArticleinSelectedDate(wikipediaPageRawName, inputDate)
+      const inputDate = document.getElementById("date-picker").value
+      await openArticleInSelectedDate(wikipediaPageRawName, inputDate)
     })
+
+    datePicker.addEventListener("input", () => {
+      today = new Date().toISOString().split('T')[0]
+      selectedDate = new Date(datePicker.value).toISOString().split('T')[0]
+      creationDate = new Date(datePicker.min).toISOString().split('T')[0]
+      if (datePicker.value && selectedDate >= creationDate && selectedDate <= today) {
+        submitButton.disabled = false;
+      } else {
+        submitButton.disabled = true;
+      }
+    })
+
+
   })
 })
 
